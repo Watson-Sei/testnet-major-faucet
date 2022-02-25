3bb572a212e1a15a3ac424fea9835177f3031a0b
Watson-Sei <seinabehack@gmail.com>
2022-02-25 23:35:58 +0900

    add ogp

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable node/no-extraneous-import */
 /* eslint-disable node/no-unpublished-import */
 /* eslint-disable no-use-before-define */
 /* eslint-disable node/no-missing-import */
@@ -5,10 +6,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "tailwindcss/tailwind.css";
 import "tw-elements";
+import { Helmet } from "react-helmet";
 import { App } from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Helmet>
+      <title>Testnet Major Faucet</title>
+      <meta
+        name="description"
+        content="Stdoneサービス向けtestnet erc20 token Faucetサイト"
+      />
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1.0,minimum-scale=1.0"
+      ></meta>
+      <head prefix="og: http://ogp.me/ns#" />
+      <meta
+        property="og:url"
+        content="https://testnet-major-faucet.vercel.app/"
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Testnet Major Faucet" />
+      <meta property="og:description" content="" />
+      <meta property="og:site_name" content="Testnet Major Faucet" />
+      <meta
+        property="og:image"
+        content="https://1.bp.blogspot.com/-Z5C0RZ6fQoY/VozfYEKr1GI/AAAAAAAA2kY/JGt6e1_y7QA/s400/mizu_suidou_stop.png"
+      />
+    </Helmet>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
