8d3c05cff835fe521f1c006d4816f27e9715c4bb
Watson-Sei <seinabehack@gmail.com>
2022-02-26 12:55:12 +0900

    add helmet provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,36 +6,38 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "tailwindcss/tailwind.css";
 import "tw-elements";
-import { Helmet } from "react-helmet-async";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import { App } from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Helmet>
-      <title>Testnet Major Faucet</title>
-      <meta
-        name="description"
-        content="Stdoneサービス向けtestnet erc20 token Faucetサイト"
-      />
-      <meta
-        name="viewport"
-        content="width=device-width,initial-scale=1.0,minimum-scale=1.0"
-      ></meta>
-      <head prefix="og: http://ogp.me/ns#" />
-      <meta
-        property="og:url"
-        content="https://testnet-major-faucet.vercel.app/"
-      />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content="Testnet Major Faucet" />
-      <meta property="og:description" content="" />
-      <meta property="og:site_name" content="Testnet Major Faucet" />
-      <meta
-        property="og:image"
-        content="https://1.bp.blogspot.com/-Z5C0RZ6fQoY/VozfYEKr1GI/AAAAAAAA2kY/JGt6e1_y7QA/s400/mizu_suidou_stop.png"
-      />
-    </Helmet>
-    <App />
+    <HelmetProvider>
+      <Helmet>
+        <title>Testnet Major Faucet</title>
+        <meta
+          name="description"
+          content="Stdoneサービス向けtestnet erc20 token Faucetサイト"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1.0,minimum-scale=1.0"
+        ></meta>
+        <head prefix="og: http://ogp.me/ns#" />
+        <meta
+          property="og:url"
+          content="https://testnet-major-faucet.vercel.app/"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Testnet Major Faucet" />
+        <meta property="og:description" content="" />
+        <meta property="og:site_name" content="Testnet Major Faucet" />
+        <meta
+          property="og:image"
+          content="https://1.bp.blogspot.com/-Z5C0RZ6fQoY/VozfYEKr1GI/AAAAAAAA2kY/JGt6e1_y7QA/s400/mizu_suidou_stop.png"
+        />
+      </Helmet>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
